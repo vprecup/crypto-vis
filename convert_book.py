acefce4035d6bc973b78458817814e206a8a51f2
Armin Weihbold <armin.weihbold@gmail.com>
2018-06-19 03:55:10 +0200

    poc full book

--- convert_book.py
+++ convert_book.py
@@ -6,31 +6,41 @@ try:
     with open('bfxbook.json') as f:
         data = json.loads(f.read())
         f.close()
+    book = []
+    minprice = 10000000.0   # adjust according to mooning
+    maxprice = 0.0
+
+    for step in data:
+        bin = step['_source']
+        sortedbin = sorted(bin['book'], key=lambda entry: entry['price'], reverse=True)
+
+        asks = [{"price": item['price'],"amount": abs(item['amount'])} for item in sortedbin if item['amount'] < 0]
+        askTotal = 0.0
+        if asks[0]['price'] > maxprice:
+            maxprice = asks[0]['price']
+        for i in range(len(asks)-1, -1, -1):
+            askTotal = askTotal + asks[i]['amount']
+            asks[i]['total'] = askTotal
+
+        bids = [{"price": item['price'],"amount": abs(item['amount'])} for item in sortedbin if item['amount'] > 0]
+        bidTotal = 0.0
+        if bids[-1]['price'] < minprice:
+            minprice = bids[-1]['price']
+        for i in range(len(bids)):
+            bidTotal = bidTotal + bids[i]['amount']
+            bids[i]['total'] = bidTotal
+
+        datum = {
+            'time': bin['localtime'],
+            'asks': sorted(asks, key=lambda e: e['price'], reverse=True),
+            'bids': sorted(bids, key=lambda e: e['price'], reverse=True)
+        }
+        book.append(datum)
+    print(json.dumps({'extent': [minprice, maxprice], 'book': book}))
 except:
     print("Error reading book data")
 
-bin = data[0]['_source']
-sortedbin = sorted(bin['book'], key=lambda entry: entry['price'], reverse=True)
 
-asks = [{"price": item['price'],"amount": abs(item['amount'])} for item in sortedbin if item['amount'] < 0]
-askTotal = 0.0
-for i in range(len(asks)-1, -1, -1):
-    askTotal = askTotal + asks[i]['amount']
-    asks[i]['total'] = askTotal
-
-bids = [{"price": item['price'],"amount": abs(item['amount'])} for item in sortedbin if item['amount'] > 0]
-bidTotal = 0.0
-for i in range(len(bids)):
-    bidTotal = bidTotal + bids[i]['amount']
-    bids[i]['total'] = bidTotal
-
-datum = {
-    'time': bin['localtime'],
-    'asks': sorted(asks, key=lambda e: e['price'], reverse=True),
-    'bids': sorted(bids, key=lambda e: e['price'], reverse=True)
-}
-
-print(json.dumps(datum))
 
 
 
